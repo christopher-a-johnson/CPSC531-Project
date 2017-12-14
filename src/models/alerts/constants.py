e3fdf76116728af862c2dba7479b638e2577681b
Christopher Johnson <christopher.johnson@cernasolutions.com>
2017-12-13 19:39:43 -0800

    models users python implementation for items and alerts

---
+++ src/models/alerts/constants.py
@@ -0,0 +1,6 @@
+
+URL = "https://api.mailgun.net/v3/sandbox29e1ff01c0be4282a0cfa1c0986a9fc7.mailgun.org/messages"
+API_KEY = "key-50ec7f3d57be9c0bc17569d6ef806ca4"
+FROM = "Mailgun Sandbox <postmaster@sandbox29e1ff01c0be4282a0cfa1c0986a9fc7.mailgun.org>"
+ALERT_TIMEOUT = 10
+COLLECTION = "alerts"
